8ac3a0016e5b656b7a010e6e4fb831a2cd80fa34
grosmichel <grosmichel417@gmail.com>
2026-09-02 15:50:56 +0900

    0902

--- 문제풀이/삼성시의 버스 노선/bus_line.py
+++ 문제풀이/삼성시의 버스 노선/bus_line.py
@@ -1,27 +1,15 @@
 import sys
-
 sys.stdin = open("s_input.txt", "r")
 
 
+def compute(P, N, AB, C):
+    result = [0] * P
+    for i in range(N):
+        temp = [idx for idx, c in enumerate(C) if AB[i][0] <= c <= AB[i][1]]
+        for idx in temp:
+            result[idx] += 1
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return result
 
 
 T = int(input())
@@ -37,4 +25,4 @@ for test_case in range(T):
     for j in range(P):
         C[j] = int(input())
 
-    print(f'#{test_case + 1}')
+    print(f'#{test_case + 1}', ' '.join(map(str, compute(P, N, AB, C))))
