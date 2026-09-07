d55e76a0c40a67bd9a9d9e142ad2b2de76b2800e
grosmichel <grosmichel417@gmail.com>
2026-09-07 13:51:23 +0900

    0907

--- 문제풀이/가장 빠른 문자열 타이핑/fest_typing.py
+++ 문제풀이/가장 빠른 문자열 타이핑/fest_typing.py
@@ -2,9 +2,10 @@ import sys
 sys.stdin = open("sample_input.txt", "r")
 
 
-
 T = int(input())
 for test_case in range(T):
-    N, M = list(map(int, input().split()))
-    L = list(map(int, input().split()))
-    print(f'#{test_case + 1}', L)
+    A, B = input().split()
+    cnt = 0
+    for i in A.split(B):
+        cnt += len(i)
+    print(f'#{test_case + 1}', len(A.split(B))+cnt-1)
