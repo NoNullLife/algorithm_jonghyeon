a76637f3b29f35eae8443c1464d93f119a97449b
grosmichel <grosmichel417@gmail.com>
2026-09-07 17:47:20 +0900

    0907

---
+++ 문제풀이/문자열 비교/compare_string.py
@@ -0,0 +1,24 @@
+import sys
+sys.stdin = open("sample_input.txt", "r")
+
+
+def find_pattern(str1, str2):
+    N = len(str1)
+    M = len(str2)
+    for i in range(M-N+1):
+        trigger = True
+        for j in range(N):
+            if str1[j] != str2[i+j]:
+                trigger = False
+                break
+        if trigger:
+            return 1
+
+    return 0
+
+
+T = int(input())
+for test_case in range(T):
+    str1 = list(input())
+    str2 = list(input())
+    print(f'#{test_case + 1}', find_pattern(str1, str2))
