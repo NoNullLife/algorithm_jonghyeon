f11092706896eec5be2350db32b8701250456df4
grosmichel <grosmichel417@gmail.com>
2026-09-08 16:29:01 +0900

    0908

--- 문제풀이/러시아 국기 같은 깃발/flag.py
+++ 문제풀이/러시아 국기 같은 깃발/flag.py
@@ -2,9 +2,21 @@ import sys
 sys.stdin = open("sample_input.txt", "r")
 
 
+def red_winter(N, M, F):
+    require_X = []
+    for i in range(N - 2):
+        for j in range(i + 1, N - 1):
+            not_X_cnt = 0
+            not_X_cnt += ''.join(F[:i + 1]).count('W')
+            not_X_cnt += ''.join(F[i + 1:j + 1]).count('B')
+            not_X_cnt += ''.join(F[j + 1:]).count('R')
+            require_X.append(N * M - not_X_cnt)
+
+    return min(require_X)
+
 
 T = int(input())
 for test_case in range(T):
     N, M = map(int, input().split())
-    L = [list(input()) for _ in range(N)]
-    print(f'#{test_case + 1}', L)
+    F = [input() for _ in range(N)]
+    print(f'#{test_case + 1}', red_winter(N, M, F))
