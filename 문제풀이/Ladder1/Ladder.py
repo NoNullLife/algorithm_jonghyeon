3f85c397625d8ed38781e7b01f908b7b96785c34
paper <paper417@naver.com>
2026-09-16 00:51:29 +0900

    0915

--- 문제풀이/Ladder1/Ladder.py
+++ 문제풀이/Ladder1/Ladder.py
@@ -2,43 +2,49 @@ import sys
 sys.stdin = open("input.txt", "r")
 
 
+dr = [-1, 0, 0]
+dc = [0, -1, 1]
+
+
+# 재귀로 안풀거임.
 def try_Ladder(M):
-    for j in range(101):
-        result = j
-        current_i = 0
-        current_j = j
-        if M[0][current_j] == 0:
-            continue
 
-        while current_i < 99:
+    # 먼저 끝 줄에서 2를 찾아봅시다
+    for idx, item in enumerate(M[-1]):
+        if item == 2:
+            current_r = 99
+            current_c = idx
+            break
 
-            if M[current_i][current_j - 1] == 1:
-                current_j = left(current_j, M[current_i])
+    # 찾았니? 그럼 이제 방향 탐색 하셔야죠?
 
-            elif M[current_i][current_j + 1] == 1:
-                current_j = right(current_j, M[current_i])
+    direction = 0  # 처음엔 위쪽 방향을 바라봅니다.
+    while current_r > 0:
+        # 위쪽 바라보면서 가다가 만약 옆에 0 아니고 1이면 어떻게 해야해요? 방향 틀어야해요.
+        # 맨 왼쪽이면 왼쪽을 검사할 필요가 없고, 맨 오른쪽이면 오른쪽을 검사할 필요가 없어요.
 
-            current_i += 1
+        if current_c > 0 and M[current_r][current_c - 1] == 1 and direction == 0:
+            direction = 1
 
-        if M[current_i][current_j] == 2:
-            return result
+        elif current_c < 99 and M[current_r][current_c + 1] == 1 and direction == 0:
+            direction = 2
+
+        # 다음 좌표를 계산해줍시다.
+        next_r, next_c = current_r + dr[direction], current_c + dc[direction]
+
+        # 범위를 벗어났거나 막혀있으면 다시 위로 향합니다.
+        if next_c < 0 or next_c > 99 or M[next_r][next_c] == 0:
+            direction = 0
+            current_r -= 1
+
+        # 아니라면 그대로 좌표를 업데이트합니다.
+        else:
+            current_r, current_c = next_r, next_c
+
+    return current_c
 
 
-def left(current_j, row):
-    for j in range(current_j, 0, -1):
-        if row[j - 1] == 0:
-            return j
-
-
-
-
-def right(current_j, row):
-    for j in range(current_j, 101):
-        if row[j + 1] == 0:
-            return j
-
-
-T = int(input())
-for test_case in range(T):
-    M = [[0]+list(map(int, input().split()))+[0] for _ in range(100)]
-    print(f'#{test_case + 1}', try_Ladder(M)-1)
+for _ in range(10):
+    T = int(input())
+    M = [list(map(int, input().split())) for _ in range(100)]
+    print(f'#{T}', try_Ladder(M))
--- 문제풀이/배열 최소 합/template.py
+++ 문제풀이/배열 최소 합/template.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.stdin = open("sample_input.txt", "r")
 
 
