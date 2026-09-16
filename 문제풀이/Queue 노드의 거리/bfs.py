f46818cff729977171c09402ed1747da10e70754
grosmichel <grosmichel417@gmail.com>
2026-09-16 17:51:25 +0900

    0916

---
+++ 문제풀이/Queue 노드의 거리/bfs.py
@@ -0,0 +1,11 @@
+import sys
+sys.stdin = open("sample_input.txt", "r")
+
+
+
+T = int(input())
+for test_case in range(T):
+    V, E = list(map(int, input().split()))
+    L = [list(map(int, input().split())) for _ in range(E)]
+    S, G = list(map(int, input().split()))
+    print(f'#{test_case + 1}', L)
