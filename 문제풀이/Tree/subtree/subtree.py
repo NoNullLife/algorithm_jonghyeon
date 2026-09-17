a0e589647fb543b01d1215ee84ca1a4ec084b722
grosmichel <grosmichel417@gmail.com>
2026-09-17 14:41:45 +0900

    0917

--- 문제풀이/Tree/subtree/subtree.py
+++ 문제풀이/Tree/subtree/subtree.py
@@ -2,13 +2,22 @@ import sys
 sys.stdin = open("sample_input.txt", "r")
 
 
+def cnt_node(graph, current_node):
+    cnt = 1
+    if graph[current_node] == []:
+        return cnt
+
+    for next_node in graph[current_node]:
+        cnt += cnt_node(graph, next_node)
+
+    return cnt
 
 
 T = int(input())
 for test_case in range(T):
     E, N = map(int, input().split())
     L = list(map(int, input().split()))
-    graph = [[] for _ in range(E+2)]
+    graph = [[] for _ in range(E + 2)]
     for i in range(E):
         graph[L[2 * i]].append(L[2 * i + 1])
-    print(f'#{test_case + 1}', graph)
+    print(f'#{test_case + 1}', cnt_node(graph, N))
